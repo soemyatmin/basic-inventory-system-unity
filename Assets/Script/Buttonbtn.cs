4986762f0dda6835315acbb9515213d536d0e854
Soemyatmin <17348039+Soemyatmin@users.noreply.github.com>
2020-06-08 15:32:39 +0630

    git ignore added

---
+++ Assets/Script/Buttonbtn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buttonbtn : MonoBehaviour {
+
+    public GameObject optionList;
+    public Sprite neutral, highlight;
+
+    private bool clicked = false;
+    private Image sprite;
+
+    void Awake() {
+        sprite = GetComponent<Image>();
+    }
+    public void Click() {
+        if (!clicked) {
+            sprite.sprite = highlight;
+            clicked = true;
+        } else {
+            sprite.sprite = neutral;
+            clicked = false;
+        }
+        optionList.SetActive(clicked);
+    }
+
+    public bool ReClicked() {
+        optionList.SetActive(clicked);
+        return clicked;
+    }
+
+}
---
+++ Assets/Script/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Item Data", order = 51)]
+public class Inventory : ScriptableObject {
+
+    public int ID;
+    public string Name;
+    public int Rank;
+    public int UnitPrice;
+    public string Desception;
+    public string Category;
+    public Sprite Image;
+
+    private int inventoryCount;
+    public Inventory(int ID, int count) {
+        this.ID = ID;
+        this.inventoryCount= count;
+    }
+
+    public void SetInventoryItem(Inventory inv) { // to fill all the information to the item list from 
+        this.Name = inv.Name;
+        this.Rank = inv.Rank;
+        this.UnitPrice = inv.UnitPrice;
+        this.Desception = inv.Desception;
+        this.Category = inv.Category;
+        this.Image = inv.Image;
+    }
+
+    public int GetInventoryCount() {
+        return inventoryCount;
+    }
+
+    public void SetInventoryCount(int value) {
+        inventoryCount = value;
+    }
+
+    public void SellItem(int value) {
+        inventoryCount -= value;
+    }
+}
